4773efb72dc3bf81053359cc8a3d06732e831410
Utkarsh Karki <utkarsh1998@icloud.com>
2024-04-02 19:35:48 -0700

    remember in composable

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package com.example.basicstatecodelab
 
+import WellnessTaskItem
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,21 +19,29 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable {
-          mutableIntStateOf(0)
-        }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if(count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if(showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minutes walk today?",
+                    onClose = { showTask = false }
+                )
+            }
             // This text is present if the button has been clicked
             // at least once; absent otherwise
             Text("You've had $count glasses.")
         }
 
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text("Add one")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
